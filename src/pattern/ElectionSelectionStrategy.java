94b3fcecc11b2fdbdf69d900481cc034a246991b
Fars <77448656+944613709@users.noreply.github.com>
2022-06-23 17:43:35 +0800

    Update ElectionSelectionStrategy.java

--- src/pattern/ElectionSelectionStrategy.java
+++ src/pattern/ElectionSelectionStrategy.java
@@ -2,14 +2,12 @@ package pattern;
 
 import auxiliary.Person;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ElectionSelectionStrategy<Person> implements SelectionStrategy<Person> {
     /**
-     * 应用 2：选择排名前?(quantity)的候选人，若有多个候选人的支持票数量相等而无
-     * 法自然排出前?名，则仅有那些明确可进入前?名的人当选；
+     * 应用 2：选择排名前?(quantity)的候选人，
+     * 若有多个候选人的支持票数量相等而无法自然排出前?名，则仅有那些明确可进入前?名的人当选；
      * @param statistics
      * @return
      */
@@ -18,7 +16,22 @@ public class ElectionSelectionStrategy<Person> implements SelectionStrategy<Pers
         //statistics的value->支持票总总数
         HashMap<Person, Double> result = new HashMap<>();
         //一般情况
-        ArrayList<Person> candidates = new ArrayList<>();
-        
+        List<Map.Entry<Person, Double>> res = new ArrayList();
+        //res里面存放着按支持票总数排列的
+        for(Map.Entry<Person, Double> entry : statistics.entrySet()) {
+            res.add(entry);
+        }
+        Collections.sort(res, new Comparator<Map.Entry<Person, Double>>(){
+            public int compare(Map.Entry<Person, Double> e1, Map.Entry<Person, Double> e2) {
+                    return e1.getValue().intValue()- e2.getValue().intValue();
+            }
+        });
+        Double Number=1.0;//排名
+        for (Map.Entry<Person, Double> entry : res) {
+            result.put(entry.getKey(), Number);
+
+            Number=Number+1;
+        }
+
     }
 }
